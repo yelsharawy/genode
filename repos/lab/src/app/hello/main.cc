cccfcf14c1ad1c710f4dc318f4434b072520993f
Yusuf Elsharawy <yelsharawy@codonics.com>
2025-05-21 17:15:04 -0400

    Timer example

--- repos/lab/src/app/hello/main.cc
+++ repos/lab/src/app/hello/main.cc
@@ -1,7 +1,33 @@
-#include <base/component.h>
-#include <base/log.h>
+ #include <base/component.h>
+ #include <base/log.h>
+ #include <timer_session/connection.h>
 
-void Component::construct(Genode::Env &)
-{
-    Genode::log("Hello world");
-}
+ namespace Hello { struct Main; }
+
+ struct Hello::Main
+ {
+   Genode::Env &_env;
+
+   Timer::Connection _timer { _env };
+
+   void _handle_timeout()
+   {
+     Genode::log("woke up at ", _timer.elapsed_ms(), " ms");
+   }
+
+   Genode::Signal_handler<Main> _timeout_handler {
+     _env.ep(), *this, &Main::_handle_timeout };
+
+   Main(Genode::Env &env) : _env(env)
+   {
+     _timer.sigh(_timeout_handler);
+     _timer.trigger_periodic(1000*1000);
+     Genode::log("component constructed");
+   }
+ };
+
+ void Component::construct(Genode::Env &env)
+ {
+   static Hello::Main main(env);
+ }
+ 
